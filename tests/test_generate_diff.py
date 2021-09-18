2cb845a4660d9cdd35cdf654e0b379ddb76951b1
dirty_hippy <piaiws@gmail.com>
2021-09-18 23:41:02 +0300

    rec

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -4,16 +4,16 @@ import os
 
 def generate_diff_by_file_ext(file_ext):
     test_dir = os.path.dirname(os.path.realpath(__file__))
-    with open(test_dir + "/fixtures/test1.txt", "r") as test1:
+    with open(test_dir + "/fixtures/simple.txt", "r") as test1:
         assert generate_diff(
-            test_dir + "/fixtures/file1.{}".format(file_ext),
-            test_dir + "/fixtures/file2.{}".format(file_ext)
+            test_dir + "/fixtures/simple1.{}".format(file_ext),
+            test_dir + "/fixtures/simple2.{}".format(file_ext)
         ) == test1.read()
 
 
-def test_json():
+def test_simple_json():
     generate_diff_by_file_ext("json")
 
 
-def test_yml():
+def test_simple_yml():
     generate_diff_by_file_ext("yml")
