8575cef3eb3ba94e96e5880a35e25dd0e55a766a
dirty_hippy <piaiws@gmail.com>
2021-09-21 01:21:27 +0300

    add fixtures

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,19 +1,28 @@
 from gendiff.scripts.gendiff import generate_diff
 import os
+import pytest
 
 
-def generate_diff_by_file_ext(file_ext):
-    test_dir = os.path.dirname(os.path.realpath(__file__))
-    with open(test_dir + "/fixtures/simple.txt", "r") as test1:
-        assert generate_diff(
-            test_dir + "/fixtures/simple1.{}".format(file_ext),
-            test_dir + "/fixtures/simple2.{}".format(file_ext)
-        ) == test1.read()
+def get_fixture_path(file_name):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(current_dir, 'fixtures', file_name)
 
 
-def test_simple_json():
-    generate_diff_by_file_ext("json")
+def read(file_path):
+    with open(file_path, 'r') as f:
+        result = f.read()
+    return result
 
 
-def test_simple_yml():
-    generate_diff_by_file_ext("yml")
+plain_json1 = get_fixture_path('plain1.json')
+plain_json2 = get_fixture_path('plain2.json')
+plain_yaml1 = get_fixture_path('plain1.yml')
+plain_yaml2 = get_fixture_path('plain2.yml')
+plain_expected = get_fixture_path('nested_expected.txt')
+plain_expected_value = read(plain_expected)
+
+
+@pytest.mark.parametrize("file1, file2", [(plain_json1, plain_json2),
+                                          (plain_yaml1, plain_yaml2)])
+def test_plain(file1, file2):
+    assert plain_expected_value == generate_diff(file1, file2)
