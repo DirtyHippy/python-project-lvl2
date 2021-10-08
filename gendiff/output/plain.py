0458735014cbbbf996c9eb0fad867809726e1d3c
dirty_hippy <piaiws@gmail.com>
2021-10-09 00:50:48 +0300

    added plain format

--- gendiff/output/plain.py
+++ gendiff/output/plain.py
@@ -1,2 +1,34 @@
-def stringify(dict: dict) -> str:
-    pass
+def format_value(value):
+    if isinstance(value, dict):
+        return "[complex value]"
+    elif isinstance(value, str):
+        return f"'{value}'"
+    elif isinstance(value, bool):
+        if value is True:
+            return "true"
+        else:
+            return "false"
+    elif value is None:
+        return "null"
+    return value
+
+
+def stringify(difference: dict) -> str:
+    result = []
+    for diff_key, diff_value in difference.items():
+        for key, val in diff_value.items():
+            line = ''
+            key = key.replace("][", '.').replace("]", "").replace("[", "")
+            if diff_key == "added":
+                line = f"'{key}' was {diff_key} with value: {format_value(val)}"
+            elif diff_key == "removed":
+                line = f"'{key}' was {diff_key}"
+            elif diff_key == 'updated':
+                orig = format_value(val['orig_value'])
+                new = format_value(val['new_value'])
+                line = f"'{key}' was {diff_key}. From {orig} to {new}"
+            if line:
+                result.append(line)
+            result.sort()
+    result = map(lambda line: 'Property ' + line, result)
+    return '\n'.join(result)
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -14,11 +14,11 @@ def read(file_path):
     return result
 
 
-plain_json1 = get_fixture_path('plain1.json')
-plain_json2 = get_fixture_path('plain2.json')
-plain_yml1 = get_fixture_path('plain1.yml')
-plain_yml2 = get_fixture_path('plain2.yml')
-plain_expected_value = read(get_fixture_path('plain.txt'))
+simple_json1 = get_fixture_path('simple1.json')
+simple_json2 = get_fixture_path('simple2.json')
+simple_yml1 = get_fixture_path('simple1.yml')
+simple_yml2 = get_fixture_path('simple2.yml')
+simple_expected_value = read(get_fixture_path('simple.txt'))
 
 
 nested_json1 = get_fixture_path('nested1.json')
@@ -26,17 +26,23 @@ nested_json2 = get_fixture_path('nested2.json')
 nested_yml1 = get_fixture_path('nested1.yaml')
 nested_yml2 = get_fixture_path('nested2.yaml')
 nested_expected_value = read(get_fixture_path('nested.txt'))
-
-
-@pytest.mark.parametrize("file1, file2", [(plain_json1, plain_json2),
-                                          (plain_yml1, plain_yml2)])
-def test_plain(file1, file2):
-    assert plain_expected_value == generate_diff(file1, file2)
-
+nested_expected_value_plain_format = read(get_fixture_path('nested_plain.txt'))
 
 '''
+@pytest.mark.parametrize("file1, file2", [(simple_json1, simple_json2),
+                                          (simple_yml1, simple_yml2)])
+def test_plain(file1, file2):
+    assert simple_expected_value == generate_diff(file1, file2)
+
+
 @pytest.mark.parametrize("file1, file2", [(nested_json1, nested_json2),
                                           (nested_yml1, nested_yml2)])
 def test_nested(file1, file2):
     assert nested_expected_value == generate_diff(file1, file2)
 '''
+
+
+@pytest.mark.parametrize("file1, file2", [(nested_json1, nested_json2)])
+def test_nested_plain_format(file1, file2):
+    assert nested_expected_value_plain_format == generate_diff(
+        file1, file2, 'plain')
