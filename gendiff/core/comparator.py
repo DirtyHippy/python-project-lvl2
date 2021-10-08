6c6933746851e2a268af3b1ea50bfca39e8b646a
dirty_hippy <piaiws@gmail.com>
2021-10-08 16:16:13 +0300

    s

--- gendiff/core/comparator.py
+++ gendiff/core/comparator.py
@@ -7,7 +7,7 @@ ADDED = 'added'
 EQUAL = 'equal'
 
 result: Dict[str, dict]
-result = {}
+result = {REMOVED: {}, UPDATED: {}, ADDED: {}, EQUAL: {}}
 
 
 def compare_dictionaries(dict_1: dict,  # noqa: C901
@@ -17,18 +17,18 @@ def compare_dictionaries(dict_1: dict,  # noqa: C901
     for k in dict_1.keys():
         path = orig_path + f"[{k}]"
         if k not in dict_2:
-            result.setdefault(REMOVED, dict())[path] = dict_1[k]
+            result[REMOVED][path] = dict_1[k]
         else:
             if isinstance(dict_1[k], dict) and isinstance(dict_2[k], dict):
                 compare_dictionaries(dict_1[k], dict_2[k], path)
             else:
                 if dict_1[k] != dict_2[k]:
-                    result.setdefault(UPDATED, dict())[path] = {
-                        'orig_value': dict_1[k], 'new_value': dict_2[k]}
+                    result[UPDATED][path] = {'orig_value': dict_1[k],
+                                             'new_value': dict_2[k]}
                 else:
-                    result.setdefault(EQUAL, dict())[path] = dict_1[k]
+                    result[EQUAL][path] = dict_1[k]
     for k in dict_2.keys():
         path = orig_path + f"[{k}]"
         if k not in dict_1:
-            result.setdefault(ADDED, dict())[path] = dict_2[k]
+            result[ADDED][path] = dict_2[k]
     return result
