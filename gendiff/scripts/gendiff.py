d2a9788d04c484a4da296459f5180c1d44112209
dirty_hippy <piaiws@gmail.com>
2021-10-10 19:34:24 +0300

    fix cli

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -17,9 +17,9 @@ def main():
                         default='stylish',
                         help='set format of output')
     args = parser.parse_args()
-    return generate_diff(args.first_file,
-                         args.second_file,
-                         args.format)
+    print(generate_diff(args.first_file,
+                        args.second_file,
+                        args.format))
 
 
 def load_files(file1: str, file2: str, load_func) -> Tuple[dict, dict]:
@@ -28,7 +28,7 @@ def load_files(file1: str, file2: str, load_func) -> Tuple[dict, dict]:
             return load_func(f1), load_func(f2)
 
 
-def show_diff(difference: dict, format: str) -> str:
+def get_diff(difference: dict, format: str) -> str:
     if format == 'plain':
         return plain.format(difference)
     elif format == 'json':
@@ -48,7 +48,7 @@ def generate_diff(file1: str, file2: str, format='stylish') -> Union[str, None]:
             *load_files(file1, file2, safe_load))
     else:
         raise Exception("wrong data format")
-    return show_diff(difference, format)
+    return get_diff(difference, format)
 
 
 if __name__ == '__main__':
