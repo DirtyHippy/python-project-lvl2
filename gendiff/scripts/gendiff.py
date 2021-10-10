7cccb3bb855d768d8135ef44d45d5904b201734c
dirty_hippy <piaiws@gmail.com>
2021-10-10 19:12:34 +0300

    fix cli

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -11,11 +11,15 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument('-f',
+                        '--format',
+                        choices=['stylish', 'plain', 'json'],
+                        default='stylish',
+                        help='set format of output')
     args = parser.parse_args()
-    return generate_diff(args['first_file'],
-                         args['second_file'],
-                         args['format'])
+    return generate_diff(args.first_file,
+                         args.second_file,
+                         args.format)
 
 
 def load_files(file1: str, file2: str, load_func) -> Tuple[dict, dict]:
