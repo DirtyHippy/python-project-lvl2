54cfc48db3710661c94d66471e9e1ee163283d04
dirty_hippy <piaiws@gmail.com>
2021-10-10 16:54:18 +0300

    added test

--- tests/test_generate_diff_2.py
+++ tests/test_generate_diff_2.py
@@ -5,7 +5,7 @@ import pytest
 
 def get_fixture_path(file_name):
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(current_dir, 'fixtures/test1', file_name)
+    return os.path.join(current_dir, 'fixtures/test2', file_name)
 
 
 def read(file_path):
@@ -14,25 +14,12 @@ def read(file_path):
     return result
 
 
-simple_json1 = get_fixture_path('simple1.json')
-simple_json2 = get_fixture_path('simple2.json')
-simple_yml1 = get_fixture_path('simple1.yml')
-simple_yml2 = get_fixture_path('simple2.yml')
-simple_expected_value = read(get_fixture_path('simple_stylish.txt'))
-
-
 nested_json1 = get_fixture_path('nested1.json')
 nested_json2 = get_fixture_path('nested2.json')
-nested_yml1 = get_fixture_path('nested1.yaml')
-nested_yml2 = get_fixture_path('nested2.yaml')
+nested_yml1 = get_fixture_path('nested1.yml')
+nested_yml2 = get_fixture_path('nested2.yml')
 nested_expected_value = read(get_fixture_path('nested_stylish.txt'))
 nested_expected_value_plain_format = read(get_fixture_path('nested_plain.txt'))
-
-
-@pytest.mark.parametrize("file1, file2", [(simple_json1, simple_json2),
-                                          (simple_yml1, simple_yml2)])
-def test_plain(file1, file2):
-    assert simple_expected_value == generate_diff(file1, file2)
 
 
 @pytest.mark.parametrize("file1, file2", [(nested_json1, nested_json2),
