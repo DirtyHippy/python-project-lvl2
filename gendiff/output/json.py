a6bc1ae5d195b7edb98b430f49b23e7ee6e28dd7
dirty_hippy <piaiws@gmail.com>
2021-10-10 19:03:01 +0300

    tests

--- gendiff/output/json.py
+++ gendiff/output/json.py
@@ -1,6 +1,23 @@
-from gendiff.output.stylish import create_dict_format
 import json
 
 
+def key_to_json(data):
+    if data is None or isinstance(data, (bool, int, str)):
+        return data
+    if isinstance(data, (tuple, frozenset)):
+        return str(data)
+    raise TypeError
+
+
+def to_json(data):
+    if data is None or isinstance(data, (bool, int, tuple, range, str, list)):
+        return data
+    if isinstance(data, (set, frozenset)):
+        return sorted(data)
+    if isinstance(data, dict):
+        return {key_to_json(key): to_json(data[key]) for key in data}
+    raise TypeError
+
+
 def format(difference: dict) -> str:
-    return json.dumps(create_dict_format(difference))
+    return json.dumps(to_json(difference))
