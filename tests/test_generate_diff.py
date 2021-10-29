91b9427231cd3fde4677e35cf16aa3de6bec8860
dirty_hippy <piaiws@gmail.com>
2021-10-29 18:01:03 +0300

    refactoring tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -5,7 +5,7 @@ from gendiff.generator import PLAIN, STYLISH, generate_diff
 
 def get_fixture_path(file_name):
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(current_dir, 'fixtures/test1', file_name)
+    return os.path.join(current_dir, 'fixtures/', file_name)
 
 
 def read(file_path):
@@ -14,35 +14,45 @@ def read(file_path):
     return result
 
 
-def simple_expected_value():
-    return read(get_fixture_path('simple_stylish.txt'))
+def simple_expected_value(test_path: str):
+    return read(get_fixture_path(f'{test_path}simple_stylish.txt'))
 
 
-def nested_expected_value():
-    return read(get_fixture_path('nested_stylish.txt'))
+def nested_expected_value(test_path: str):
+    return read(get_fixture_path(f'{test_path}nested_stylish.txt'))
 
 
-def nested_expected_value_plain_format():
-    return read(get_fixture_path('nested_plain.txt'))
+def nested_expected_value_plain_format(test_path: str):
+    return read(get_fixture_path(f'{test_path}nested_plain.txt'))
 
 
-simple_json1 = get_fixture_path('simple1.json')
-simple_json2 = get_fixture_path('simple2.json')
-simple_yml1 = get_fixture_path('simple1.yml')
-simple_yml2 = get_fixture_path('simple2.yml')
+simple_json1 = 'simple1.json'
+simple_json2 = 'simple2.json'
+simple_yml1 = 'simple1.yml'
+simple_yml2 = 'simple2.yml'
 
 
-nested_json1 = get_fixture_path('nested1.json')
-nested_json2 = get_fixture_path('nested2.json')
-nested_yml1 = get_fixture_path('nested1.yaml')
-nested_yml2 = get_fixture_path('nested2.yaml')
+nested_json1 = 'nested1.json'
+nested_json2 = 'nested2.json'
+nested_yml1 = 'nested1.yaml'
+nested_yml2 = 'nested2.yaml'
+
+test1_path = 'test1/'
+test2_path = 'test2/'
 
 
-@ pytest.mark.parametrize("file1, file2, test_function, format", [(simple_json1, simple_json2, simple_expected_value, STYLISH),               # noqa E501
-                                                                 (simple_yml1, simple_yml2, simple_expected_value, STYLISH),                  # noqa E501
-                                                                 (nested_json1, nested_json2, nested_expected_value, STYLISH),                # noqa E501
-                                                                 (nested_yml1, nested_yml2, nested_expected_value, STYLISH),                  # noqa E501
-                                                                 (nested_json1, nested_json2, nested_expected_value_plain_format, PLAIN),     # noqa E501
-                                                                 (nested_yml1, nested_yml2, nested_expected_value_plain_format, PLAIN)])      # noqa E501
-def test_generate_diff(file1, file2, test_function, format):
-    assert test_function() == generate_diff(file1, file2, format)
+@ pytest.mark.parametrize("file1, file2, test_path, test_function, format", [
+                            (simple_json1, simple_json2, test1_path, simple_expected_value, STYLISH),              # noqa E501
+                            (simple_yml1, simple_yml2, test1_path, simple_expected_value, STYLISH),                # noqa E501
+                            (nested_json1, nested_json2, test1_path, nested_expected_value, STYLISH),              # noqa E501
+                            (nested_yml1, nested_yml2, test1_path, nested_expected_value, STYLISH),                # noqa E501
+                            (nested_json1, nested_json2, test1_path, nested_expected_value_plain_format, PLAIN),   # noqa E501
+                            (nested_yml1, nested_yml2, test1_path, nested_expected_value_plain_format, PLAIN),     # noqa E501
+                            (nested_json1, nested_json2, test2_path, nested_expected_value, STYLISH),              # noqa E501
+                            (nested_yml1, nested_yml2, test2_path, nested_expected_value, STYLISH),                # noqa E501
+                            (nested_json1, nested_json2, test2_path, nested_expected_value_plain_format, PLAIN),   # noqa E501
+                            (nested_yml1, nested_yml2, test2_path, nested_expected_value_plain_format, PLAIN)])    # noqa E501
+def test_generate_diff(file1, file2, test_path, test_function, format):
+    assert test_function(test_path) == generate_diff(get_fixture_path(test_path + file1),
+                                                     get_fixture_path(test_path + file2),
+                                                     format)
